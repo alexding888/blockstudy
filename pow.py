9c8380eea600b864d1c0dcf8f6e99bb98e83781d
alexding888 <ding@weichain.com.cn>
2024-04-16 09:45:04 +0800

    rename

---
+++ pow.py
@@ -0,0 +1,23 @@
+import flask
+import hashlib
+import time
+
+def calculate_hash_with_nonce(nonce):
+    return hashlib.sha256(('alex' + str(nonce)).encode('utf-8')).hexdigest()
+
+def find_hash_with_zeros(zeros):
+    nonce = 0
+    start_time = time.time()
+    while True:
+        hash_result = calculate_hash_with_nonce(nonce)
+        if hash_result.startswith('0' * zeros):
+            end_time = time.time()
+            print(f"Found hash with {zeros} zeros: {hash_result}")
+            print(f"Time taken: {end_time - start_time} seconds")
+            break
+        nonce += 1
+
+find_hash_with_zeros(4)
+find_hash_with_zeros(5)
+
+import flask
