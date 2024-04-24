4d0b3446406d01a8baff27e2b10bd2a9191c59dc
alexding888 <ding@weichain.com.cn>
2024-04-24 14:53:10 +0800

    newERC721

---
+++ Issue_ERC20_token.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract BaseERC20 {
+    string public name; 
+    string public symbol; 
+    uint8 public decimals; 
+
+    uint256 public totalSupply; 
+
+    mapping (address => uint256) balances; 
+
+    mapping (address => mapping (address => uint256)) allowances; 
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor()  {
+        // write your code here
+        // set name,symbol,decimals,totalSupply
+        name = "Accountingchain";
+        symbol = "DEA";
+        decimals = 18;
+        totalSupply = 1000000000 ether;
+
+        balances[msg.sender] = totalSupply;  
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        // write your code here
+        balance = balances[_owner];
+
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // write your code here
+        require(balances[msg.sender] >= _value, "ERC20: transfer amount exceeds balance");
+
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);  
+        return true;   
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        // write your code here
+        require(balances[_from]  >= _value, "ERC20: transfer amount exceeds balance");
+        require(allowances[_from][msg.sender] >= _value, "ERC20: transfer amount exceeds allowance");
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        allowances[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value); 
+        return true; 
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        // write your code here
+        allowances[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); 
+        return true; 
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {   
+        // write your code here     
+        remaining = allowances[_owner][_spender];
+    }
+}
