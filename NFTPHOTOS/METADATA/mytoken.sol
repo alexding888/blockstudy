c8395105e565e8eb7e2a7d38720e398417276794
alexding888 <ding@weichain.com.cn>
2024-04-24 14:54:15 +0800

    ERC721

---
+++ NFTPHOTOS/METADATA/mytoken.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+interface TokenRecipient {
+    function tokensReceived(address sender, uint amount) external returns (bool);
+}
+
+contract MyERC20Callback is ERC20 {
+    using Address for address;
+
+    constructor() ERC20("MyERC20", "MyERC20") {
+        _mint(msg.sender, 1000 * 10 ** 18);
+    }
+
+
+    function transferWithCallback(address recipient, uint256 amount) external returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+
+        if (recipient.code.length > 0 ) {
+            bool rv = TokenRecipient(recipient).tokensReceived(msg.sender, amount);
+            require(rv, "No tokensReceived");
+        }
+
+        return true;
+    }
+
+
+}
